55f51597bc75ef28c69262bdfcad4f0224ad9cb5
Nitin Kumar <kumar.nk.nitin77@gmail.com>
2025-06-17 11:52:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-01"
+  
+}
+
+module "ec2_instance" {
+    source = "./modules/ec2_instance"
+    subnet_id_value = "subnet-03f3cd32b85ade149"
+    ami_value = "ami-020cba7c55df1f615"
+    instance_type_value = "t2.micro"
+    key_name_value = "asible"
+}
