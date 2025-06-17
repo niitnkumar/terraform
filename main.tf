35e8ad9cc1a61f3c0e307c7a77dd7eb4171fca6b
Nitin Kumar <kumar.nk.nitin77@gmail.com>
2025-06-17 11:54:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-    source = "./modules/ec2_instance"
+    source = "./ec2_instance"
     subnet_id_value = "subnet-03f3cd32b85ade149"
     ami_value = "ami-020cba7c55df1f615"
     instance_type_value = "t2.micro"
