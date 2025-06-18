807aa5ad719ee056a46259ad82b388f042cb8fcd
Nitin Kumar <kumar.nk.nitin77@gmail.com>
2025-06-18 11:46:28 +0530

    Update backend.tf
    with db lock

--- terra-with-beckend1/terra-with-beckend/backend.tf
+++ terra-with-beckend1/terra-with-beckend/backend.tf
@@ -1,7 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-xyz"
-    key    = "nitin/terraform.tfstate"
+    bucket = "terraform-bucket-xyz" # name of s3 bucket
+    key    = "nitin/terraform.tfstate" # path of state file save in bucket
     region = "us-east-1"
+    encrypt = "true"
+    dynamodb_table = "terraform-lock"
   }
 }
+
+
